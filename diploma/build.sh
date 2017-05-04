e8545634162b4b1d0aa38858c4b070edc2c1f029
leonid <leonid.stashevsky@gmail.com>
2017-05-04 10:14:24 +0300

    buildscript

---
+++ diploma/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+xelatex -interaction=nonstopmode -shell-escape main
+bibtex main
+xelatex -interaction=nonstopmode -shell-escape main
+xelatex -interaction=nonstopmode -shell-escape main
+
+rm *.bbl *.out *.log *.toc *.aux *.blg
+rm -rf _minted-main
